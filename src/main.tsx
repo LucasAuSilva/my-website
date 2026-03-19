ac7d08670c53100e40f9e7ad1aaa25140008b235
LucasAuSilva <silvaaugustolucas@gmail.com>
2026-03-19 11:59:14 -0300

    fix: remove prerender for now

--- src/main.tsx
+++ src/main.tsx
@@ -1,63 +1,25 @@
 import { StrictMode } from 'react'
-import { createRoot, hydrateRoot } from 'react-dom/client'
-import { RouterProvider, createMemoryHistory, createRouter } from '@tanstack/react-router'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-// Styles with tailwind
+// Styles for tailwind
 import './index.css'
 
-// Translations for project
+// Translation for website
 import './i18n'
 
-function makeRouter(url?: string) {
-  return createRouter({
-    routeTree,
-    history: url
-      ? createMemoryHistory({ initialEntries: [url] })
-      : undefined,
-    isServer: !!url
-  })
-}
+// router creation
+const router = createRouter({ routeTree })
 
-// Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
-    router: ReturnType<typeof makeRouter>
+    router: typeof router
   }
 }
 
-// Render the app
-if (typeof window !== 'undefined') {
-  const router = makeRouter()
-  const root = document.getElementById('root')!
-
-  if (root.innerHTML.trim()) {
-    hydrateRoot(root, (
-      <StrictMode>
-        <RouterProvider router={router} />
-      </StrictMode>
-    ))
-  } else {
-    createRoot(root).render(
-      <StrictMode>
-        <RouterProvider router={router} />
-      </StrictMode>
-    )
-  }
-}
-
-// prerender function
-export async function prerender(data: { url: string }) {
-  const { renderToString } = await import('react-dom/server')
-
-  const router = makeRouter(data.url)
-  await router.load()
-
-  const html = renderToString(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  )
-
-  return { html }
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 import { fileURLToPath } from 'node:url'
 
@@ -16,10 +15,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    vitePrerenderPlugin({
-      prerenderScript: fileURLToPath(new URL('./src/main.tsx', import.meta.url)),
-      additionalPrerenderRoutes: ['/about', '/work', '/blog', '/contact'],
-    }),
   ],
   resolve: {
     alias: {
